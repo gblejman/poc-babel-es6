4eb5413923ca3dce9604ed45a1bbe92fff075e6e
German Blejman <gblejman@gmail.com>
2015-06-24 01:17:14 -0300

    Add test for source

---
+++ test/index.js
@@ -0,0 +1,12 @@
+import test from 'tape';
+import hello from '../source/index';
+
+test('Tests run', (assert) => {
+  assert.pass('Tests run');
+  assert.end();
+});
+
+test('Greet World', (assert) => {
+  assert.equal(hello('World'), 'Hello, World!');
+  assert.end();
+});
